7e4fe1d9e8cbf6a8a7f70e5c1f8c45223131ba74
BhautikDavariya <bhautikmerndevelopers@gmail.com>
2022-06-19 17:25:00 +0530

    HTML + Css
    HTML + CSS

---
+++ Css task/Psd To Html-Css/Templates/Template_3/data typs.js
@@ -0,0 +1,26 @@
+// Data types 
+
+let x = 'Bhautik';
+    y = 'Bhau"tik';
+
+console.log(x);
+console.log(y);
+
+// typeof 
+
+let a = 5, b = "bhautik";
+
+console.log("")
+console.log(typeof a);
+console.log(typeof b);
+
+// boolean number 
+
+let c = 2 > 3, d = 2 < 3;
+
+console.log("");
+console.log(c);
+console.log(d);
+
+
+
